b49140c4da74778a01071d59f08f228974a81a77
Skalyan <kalyansurya403@gmail.com>
2025-09-09 16:32:40 +0000

    Updated the Product and User ports

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://34.23.196.162:5762/api'
-const usersServiceDevHost = 'http://34.23.196.162:5763/api'
+const produtsServiceDevHost = 'http://34.23.196.162:6762/api'
+const usersServiceDevHost = 'http://34.23.196.162:6763/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
